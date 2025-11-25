3e1626caf723af6dc65bd5f5b726340d3016340e
free-yondu <project.greenist21@gmail.com>
2025-11-25 22:39:59 +0900

    Modify - storage class 설정

--- 05-kubernetes-storage/05-handson.sh
+++ 05-kubernetes-storage/05-handson.sh
@@ -1,11 +1,13 @@
 # empty dir 실습
+cat emptydir.yaml
 kubectl apply -f emptydir.yaml
-kubectl get pods
+kubectl get pods,svc
 
 curl $(kubectl get svc emptydir-svc -o jsonpath='{.spec.clusterIP}{"\n"}')
 
 kubectl exec -it emptydir-pod -c busybox -- sh
 echo "change content" > /home/html/index.html
+cat /home/html/index.html
 exit
 
 curl $(kubectl get svc emptydir-svc -o jsonpath='{.spec.clusterIP}{"\n"}')
@@ -17,7 +19,8 @@ mkdir -p /data/static-web
 
 echo '<h1>Hello from HostPath!</h1><p>This file is on the Kubernetes Node, not inside the Container.</p>' | sudo tee /data/static-web/index.html
 
-mv hostpath.yaml /etc/kubernetes/manifests/
+cat hostpath.yaml
+cp hostpath.yaml /etc/kubernetes/manifests/
 
 POD_NAME=$(kubectl get pods | grep hostpath-pod | awk '{print $1}')
 curl $(kubectl get pod $POD_NAME -o jsonpath='{.status.podIP}{"\n"}')
@@ -26,7 +29,7 @@ echo "change content of hostpath pod" | sudo tee /data/static-web/index.html
 
 curl $(kubectl get pod $POD_NAME -o jsonpath='{.status.podIP}{"\n"}')
 
-kubectl delete -f hostpath.yaml
+rm /etc/kubernetes/manifests/hostpath.yaml
 
 # configmap 실습
 kubectl apply -f configmap.yaml
@@ -53,8 +56,10 @@ kubectl apply -f secret.yaml
 kubectl get secret
 kubectl describe secret test-secret
 kubectl get secret test-secret -o yaml
+echo "bXktYXBw" | base64 -d
+echo "Mzk1MjgkdmRnN0pi" | base64 -d
 
-kubectl get pods
+kubectl get pods,secret
 kubectl exec -it secret-test-pod -c test-container -- sh
 
 ls -al /etc/secret-volume
@@ -71,11 +76,15 @@ kubectl get storageclass
 kubectl apply -f pvc.yaml
 kubectl apply -f pvc-pod.yaml
 kubectl get pods,pvc -o wide
+PVC_POD_IP=$(kubectl get pod pvc-pod -o jsonpath='{.status.podIP}{"\n"}')
+curl $PVC_POD_IP
 
 kubectl exec -it pvc-pod -- /bin/bash
-echo "Test Content" > /var/www/html/index.html
-cat /var/www/html/index.html
+echo "Test Content" > /usr/share/nginx/html/index.html
+cat /usr/share/nginx/html/index.html
 exit
+
+curl $PVC_POD_IP
 
 kubectl delete -f pvc-pod.yaml
 kubectl get pv,pvc
@@ -85,7 +94,7 @@ kubectl get pods,pvc -o wide
 
 curl $(kubectl get pod pvc-other-pod -o jsonpath='{.status.podIP}{"\n"}')
 kubectl exec -it pvc-other-pod -- /bin/bash
-cat /var/www/html/index.html
+cat /usr/share/nginx/html/index.html
 exit
 
 kubectl delete -f pvc-other-pod.yaml
